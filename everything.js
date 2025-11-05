cc69e4c075a77f9ed13d25d53672094cc9a9c343
null_pointer_lover <152438889+Crazy1111j@users.noreply.github.com>
2025-11-05 16:59:21 -0500

    Update everything.js

--- everything.js
+++ everything.js
@@ -1,1 +1,5 @@
-function wipeout(){document.body.innerHTML = '<object type="text/html" data="https://daves-hot-chicken-new-mexico2.uk/something.html" style="width: 100vw; height: 100vh;"></object>';}
+document.addEventListener('keydown', function(event) {
+    if (event.key === '1') {
+      document.body.innerHTML = '<object type="text/html" data="https://daves-hot-chicken-new-mexico2.uk/something.html" style="width: 100vw; height: 100vh;"></object>';
+    }
+});
